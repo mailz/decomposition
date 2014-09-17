589a5b478e0dd4548082e91f1ea288811347ee66
Dmitriy Trandin <dt@simpnet.org>
2014-09-18 01:03:59 +0400

    work

--- src/decomposition/Decomposition.java
+++ src/decomposition/Decomposition.java
@@ -5,6 +5,8 @@
  */
 package decomposition;
 
+import static java.lang.System.exit;
+
 /**
  *
  * @author ubick
@@ -15,7 +17,27 @@ public class Decomposition {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+       int first = Integer.parseInt(args[0]);
+       int second = Integer.parseInt(args[1]);
+       if(first>150){
+           exit(1);
+       }
+       int result = decomposition(first, second);
+       System.out.println(result);
+        
     }
+    
+    public static int decomposition(int n, int k){
+        if(n == k){ return 1;}
+        if(n < k){ return 0;}
+        if(k == 0){ return 0;}
+        int S = 0;
+        for(int i = 0; i<=k;i++){
+            S = S + decomposition(n-k,i);
+        }
+        return S;
+    }
+    
     
 }
